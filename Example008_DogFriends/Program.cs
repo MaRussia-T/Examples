2fa1f349bef9b855ecc6affd77bfa35b87108917
Maria <grigoryeva.m.s@gmail.com>
2022-08-04 11:51:11 +0300

    add Example 009 add Example 008 DogFriend

---
+++ Example008_DogFriends/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine("Введите скорость первого друга");
+int firstFriendSpeed = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость второго друга");
+int secondFriendSpeed = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость собаки");
+int dogSpeed = Convert.ToInt32(Console.ReadLine());
+int distance = 10000;
+int friend = 2;
+int count = 0;
+
+while (distance > 10)
+{
+    if (friend != 2)
+    {
+        int distance = 10000;
+        int time = distance / (secondFriendSpeed + dogSpeed);
+        int distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        count++;
+    }
+    else
+    {
+        int time = distanse / (firstFriendSpeed + dogSpeed);
+        int friend = 2;
+        int distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        count++;
+    }
+}
+Console.WriteLine("Собака пробежит", count, "раз");
---
+++ Example009/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Результат");
+Console.WriteLine(a + b);*/
+
+
+
+
+int resul1 = 0;
+int resul2 = 0;
+int crat = 2;
+
+while (true)
+{
+    Console.WriteLine("Введите число или команду");
+    string res = Console.ReadLine();
+    if (res == "end")
+    {
+        break;
+    }
+    else if (res == "num")
+    {
+        Console.WriteLine("Введите кратное число");
+        crat = Convert.ToInt32(Console.ReadLine());
+        resul1 = 0;
+        resul2 = 0;
+    }
+    else
+    {
+        int number = Convert.ToInt32(res);
+        if (number % 2 == 0)
+        {
+            resul1 += number;
+        }
+        else
+        {
+            resul2 += number;
+        }
+
+    }
+}
+Console.WriteLine(resul1.ToString() + "Сумма кратных чисел");
+
+Console.WriteLine(resul2.ToString() + "Сумма не кратных чисел");
+Console.WriteLine(crat.ToString() + "Кратное число");
+
