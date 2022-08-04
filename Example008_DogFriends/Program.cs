45aea9d73a22af935186edd5aa4b39cfb162ab83
Maria <grigoryeva.m.s@gmail.com>
2022-08-04 20:26:05 +0300

    add HomeWork_C#; sloved tasks 2,4,6,8

--- Example008_DogFriends/Program.cs
+++ Example008_DogFriends/Program.cs
@@ -2,32 +2,25 @@
 
 int count = 0;
 int friend = 2;
-//Console.WriteLine("Введите скорость первого друга");
-int firstFriendSpeed = 1; //Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("Введите скорость второго друга");
-int secondFriendSpeed = 2; //Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("Введите скорость собаки");
-int dogSpeed = 5; //Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("Задайте дистанцию");
- //Convert.ToInt32(Console.ReadLine());
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
 int time = 0;
-
 
 for (int distance = 10000; distance > 10; count++)
 {
-    
     if (friend == 2)
     {
         time = distance / (secondFriendSpeed + dogSpeed);
         distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
     }
-    
-    else if(friend == 1)
+
+    else if (friend == 1)
     {
         time = distance / (firstFriendSpeed + dogSpeed);
         distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
     }
-    
+
 }
 Console.Write("Собака пробежит ");
 Console.Write(count);
---
+++ HomeWork_C#/Program.cs
@@ -0,0 +1,215 @@
+﻿Console.Clear(); //очистка консоли
+
+Console.WriteLine("Задача 2"); //На экран выводится номер задачи
+//При необходимости можно предложить пользователю ввести числа из консоли, для этого необходимо заменить в строках ниже присвоение переменных кодом, прописанным после двойного слеша
+
+
+int a = 5; //переменной а присваиваем значение 5 //int a = 0; Console.WriteLine("Введите число a"); a = Convert.ToInt32(Console.ReadLine());
+int b = 7; //переменной b присваиваем значение 7 //int b = 0; Console.WriteLine("Введите число b"); b = Convert.ToInt32(Console.ReadLine());
+int max = a; //переменной max присваиваем значение равное а
+int min = b; //переменной min присваиваем значение равное b
+
+if (a > b) //если a больше b
+{
+    Console.WriteLine("Большее значение равно "); //на экран выводится строка с текстом "Большее значение равно "
+    Console.WriteLine(max = a); //На экран выводится значение переменной max
+    Console.WriteLine("Меньшее значение равно "); //на экран выводится строка с текстом "Меньшее значение равно "
+    Console.WriteLine(min = b); //На экран выводится значение переменной min
+}
+else
+{
+    Console.WriteLine("Большее значение равно "); //на экран выводится строка с текстом "Большее значение равно "
+    Console.WriteLine(max = b); //На экран выводится значение переменной max
+    Console.WriteLine("Меньшее значение равно "); //на экран выводится строка с текстом "Меньшее значение равно "
+    Console.WriteLine(min = a); //На экран выводится значение переменной min
+}
+
+
+Console.WriteLine("_______"); //отделение блока задач друг от друга
+
+
+int a1 = 2; //переменной а1 присваиваем значение 2
+int b1 = 10; //переменной b1 присваиваем значение 10
+int max1 = a1; //переменной max присваиваем значение равное а1
+int min1 = b1; //переменной min присваиваем значение равное b1
+
+if (a1 > b1) //если a1 больше b1
+{
+    Console.WriteLine("Большее значение равно "); //на экран выводится строка с текстом "Большее значение равно "
+    Console.WriteLine(max1 = a1); //На экран выводится значение переменной max1
+    Console.WriteLine("Меньшее значение равно "); //на экран выводится строка с текстом "Меньшее значение равно "
+    Console.WriteLine(min1 = b1); //На экран выводится значение переменной min1
+}
+else
+{
+    Console.WriteLine("Большее значение равно "); //на экран выводится строка с текстом "Большее значение равно "
+    Console.WriteLine(max1 = b1); //На экран выводится значение переменной max1
+    Console.WriteLine("Меньшее значение равно "); //на экран выводится строка с текстом "Меньшее значение равно "
+    Console.WriteLine(min1 = a1); //На экран выводится значение переменной min1
+}
+
+
+Console.WriteLine("_______"); //отделение блока задач друг от друга
+
+
+int a2 = -9; //переменной а присваиваем значение 5
+int b2 = -3; //переменной b присваиваем значение 7
+int max2 = a2; //переменной max2 присваиваем значение равное а2
+int min2 = b2; //переменной min2 присваиваем значение равное b2
+
+if (a2 > b2) //если a2 больше b2
+{
+    Console.WriteLine("Большее значение равно "); //на экран выводится строка с текстом "Большее значение равно "
+    Console.WriteLine(max2 = a2); //На экран выводится значение переменной max2
+    Console.WriteLine("Меньшее значение равно "); //на экран выводится строка с текстом "Меньшее значение равно "
+    Console.WriteLine(min2 = b2); //На экран выводится значение переменной min2
+}
+else
+{
+    Console.WriteLine("Большее значение равно "); //на экран выводится строка с текстом "Большее значение равно "
+    Console.WriteLine(max2 = b2); //На экран выводится значение переменной max2
+    Console.WriteLine("Меньшее значение равно "); //на экран выводится строка с текстом "Меньшее значение равно "
+    Console.WriteLine(min2 = a2); //На экран выводится значение переменной min2
+}
+
+
+Console.WriteLine("_______________________________________________________________________________");
+
+
+Console.WriteLine("Задача 4"); //На экран выводится номер задачи
+//При необходимости можно предложить пользователю ввести числа из консоли, для этого необходимо заменить в строках ниже присвоение переменных кодом, прописанным после двойного слеша
+
+int numA = 2; //int numA = 0; Console.WriteLine("Введите число numA"); numA = Convert.ToInt32(Console.ReadLine());
+int numB = 3; //int numB = 0; Console.WriteLine("Введите число numB"); numB = Convert.ToInt32(Console.ReadLine());
+int numC = 7; //int numC = 0; Console.WriteLine("Введите число numC"); numC = Convert.ToInt32(Console.ReadLine());
+int maximum = numA; //присваиваем  максимуму значение переменной numA
+
+if (numB > maximum) //если переменная numB больше максимума
+{
+    maximum = numB; //присваиваем  максимуму значение переменной numВ
+}
+if (numC > maximum) //если переменная numС больше максимума
+{
+    maximum = numC; //присваиваем  максимуму значение переменной numC
+}
+Console.WriteLine("Максимальное число равно "); //на экран выводится строка с текстом "Максимальное число равно "
+Console.WriteLine(maximum); //На экран выводится значение переменной maximum
+
+Console.WriteLine("_______");
+
+int numA1 = 44;
+int numB1 = 5;
+int numC1 = 78;
+int maximum1 = numA1;
+
+if (numB1 > maximum1)
+{
+    maximum1 = numB1;
+}
+if (numC1 > maximum1)
+{
+    maximum1 = numC1;
+}
+Console.WriteLine("Максимальное число равно ");
+Console.WriteLine(maximum1);
+
+Console.WriteLine("_______");
+
+int numA2 = 22;
+int numB2 = 3;
+int numC2 = 9;
+int maximum2 = numA2;
+
+if (numB2 > maximum2)
+{
+    maximum2 = numB2;
+}
+if (numC2 > maximum2)
+{
+    maximum2 = numC2;
+}
+Console.WriteLine("Максимальное число равно ");
+Console.WriteLine(maximum2);
+
+Console.WriteLine("_______________________________________________________________________________");
+
+Console.WriteLine("Задача 6"); //На экран выводится номер задачи
+//При необходимости можно предложить пользователю ввести числа из консоли, для этого необходимо заменить в строках ниже присвоение переменных кодом, прописанным после двойного слеша
+
+int number1 = 4; //int number1 = 0; Console.WriteLine("Введите число number1"); number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = -3; //int number2 = 0; Console.WriteLine("Введите число number1"); number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = 7; //int number3 = 0; Console.WriteLine("Введите число number1"); number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2 == 0)
+{
+    Console.Write("Число ");
+    Console.Write(number1);
+    Console.WriteLine(" четное");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(number1);
+    Console.WriteLine(" нечетное");
+}
+if (number2 % 2 == 0)
+{
+    Console.Write("Число ");
+    Console.Write(number1);
+    Console.WriteLine(" четное");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(number2);
+    Console.WriteLine(" нечетное");
+}
+if (number3 % 2 == 0)
+{
+    Console.Write("Число ");
+    Console.Write(number3);
+    Console.WriteLine(" четное");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(number3);
+    Console.WriteLine(" нечетное");
+}
+
+Console.WriteLine("_______________________________________________________________________________");
+
+Console.WriteLine("Задача 8");
+//При необходимости можно предложить пользователю ввести числа из консоли, для этого необходимо заменить в строках ниже присвоение переменных кодом, прописанным после двойного слеша
+
+int n = 0;
+int what = 1;
+n = 5; //Console.WriteLine("Введите число n"); //n = Convert.ToInt32(Console.ReadLine());
+
+while (what <= n)
+{
+    if (what % 2 == 0)
+    {
+        Console.WriteLine(what);
+    }
+
+    what++;
+}
+
+Console.WriteLine("_______");
+
+int n1 = 0;
+int what1 = 1;
+n1 = 8; //Console.WriteLine("Введите число n1"); //n1 = Convert.ToInt32(Console.ReadLine());
+
+while (what1 <= n1)
+{
+    if (what1 % 2 == 0)
+    {
+        Console.WriteLine(what1);
+    }
+
+    what1++;
+}
+
+Console.WriteLine("_______________________________________________________________________________");
