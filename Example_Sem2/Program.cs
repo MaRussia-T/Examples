001ae27c17f203aec8057f2336fa8b064ea12c50
Maria <grigoryeva.m.s@gmail.com>
2022-08-11 11:59:42 +0300

    add Example Sem2

--- Example_Sem2/Program.cs
+++ Example_Sem2/Program.cs
@@ -15,7 +15,23 @@
     };
 }
 
+class InRase
+{
+    public string Name;
+    public string Ability;
+    public string Bonus;
+    public string Side;
 
+    Dictionary<string, int> Skills = new Dictionary<string, int>()
+    {
+        {"Сила",0},
+        {"Удача",0},
+        {"Ловкость",0},
+        {"Харизма",0},
+        {"Эмпатия",0},
+        {"Красноречие",0},
+    };
+}
 
 
 
